ed9e164e95b06601a0c627d4ffdfd4d7d0aa3b6f
Tong Sun <suntong@cpan.org>
2023-08-05 21:13:40 -0400

    - [+] dup aws-0B1-algnhsa as aws-0B2-algnhsa

---
+++ aws-0B2-algnhsa/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/akrylysov/algnhsa"
+)
+
+func addHandler(w http.ResponseWriter, r *http.Request) {
+	f, _ := strconv.Atoi(r.FormValue("first"))
+	s, _ := strconv.Atoi(r.FormValue("second"))
+	w.Header().Set("X-Hi", "foo")
+	fmt.Fprintf(w, "%d", f+s)
+}
+
+func contextHandler(w http.ResponseWriter, r *http.Request) {
+	lambdaEvent, ok := algnhsa.APIGatewayV2RequestFromContext(r.Context())
+	if ok {
+		fmt.Fprint(w, lambdaEvent.RequestContext.AccountID)
+	}
+}
+
+func main() {
+	http.HandleFunc("/add", addHandler)
+	http.HandleFunc("/context", contextHandler)
+	algnhsa.ListenAndServe(http.DefaultServeMux, nil)
+}
