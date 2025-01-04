45a8b2a8fc3b6c51dc126399931778fb78ec68f3
Tong Sun <suntong@cpan.org>
2025-01-04 10:14:10 +0800

    - [+] dup aws-0E2-funcurl as-is from 0E1, go-demos_aws-0E1-funcurl

---
+++ aws-0E2-funcurl/funcdef.sh
@@ -0,0 +1,3 @@
+service=go-demos_aws-0E1-funcurl
+description=aws-lambda-go-demos/aws-0E1-funcurl
+timeout=2
---
+++ aws-0E2-funcurl/go.mod
@@ -0,0 +1,5 @@
+module aws-0E1-funcurl
+
+go 1.22.2
+
+require github.com/aws/aws-lambda-go v1.47.0
